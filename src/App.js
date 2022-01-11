c81daf8c3f39aeedc87046e4d58653f341956987
Flavio Vieira <flavidev@gmail.com>
2022-01-11 21:32:35 +0400

    added auth and styled background

--- src/App.js
+++ src/App.js
@@ -4,15 +4,28 @@ import './App.css';
 
 function App() {
   return (
-    <Authenticator>
-      {({ signOut, user }) => (
-        <main>
-          <h1>Hello {user.username}</h1>
-          <button onClick={signOut}>Sign out</button>
-        </main>
-      )}
-    </Authenticator>
+    <div className='container' style={styles.container}>
+      <Authenticator loginMechanisms={['email']}>
+        {({ signOut, user }) => (
+          <main>
+            <h1>Hello {user.username}</h1>
+            <button onClick={signOut}>Sign out</button>
+          </main>
+        )}
+      </Authenticator>
+    </div>
   );
+}
+
+const styles = {
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+    width: '100vw',
+    backgroundColor: '#225464',
+  },
 }
 
 export default App;
