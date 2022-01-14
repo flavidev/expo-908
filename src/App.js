95686ca5b7b83726c9a30e609b1419216968ce09
Flavio Vieira <flavidev@gmail.com>
2022-01-14 20:37:54 +0400

    removed i18 translatios

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import { I18n } from 'aws-amplify';
+//import { I18n } from 'aws-amplify';
 import { AmplifyProvider, Authenticator, Heading } from '@aws-amplify/ui-react';
 
-import { translations } from '@aws-amplify/ui';
-import { dictionaire } from './utils/Translations'
+//import { translations } from '@aws-amplify/ui';
+//import { dictionaire } from './utils/Translations'
 
 import Main from './pages/Main'
-
+import './App.css';
 import '@aws-amplify/ui-react/styles.css';
 
-I18n.putVocabularies(translations);
-I18n.setLanguage('pt-BR');
-I18n.putVocabularies(dictionaire)
+//I18n.putVocabularies(translations);
+//I18n.setLanguage('pt-BR');
+//I18n.putVocabularies(dictionaire)
 
 
 function App() {
@@ -62,29 +62,3 @@ const styles = {
 
 export default App;
 
-/*
-
-.welcome-text {
-  font-family: 'Azonix';
-  color: #fff;
-  font-size: 1.5rem;
-}
-
-.amplify-button[data-variation='link'] {
-  color: #8055a4;
-}
-
-.amplify-button[data-variation='primary'] {
-  background-color: #8055a4;
-  border-color: 'grey';
-  color: #fff;
-}
-
-.amplify-tabs-item[data-state='active'] {
-  color: #8055a4;
-  background-color: 'grey';
-  transition-property: none;
-  border-color: #000;
-}
-
-*/
