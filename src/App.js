85a33dd5f7e500903052a622a75a4cee286232e3
Flavio Vieira <flavidev@gmail.com>
2022-01-21 02:12:43 +0400

    fixed css issues

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import { AmplifyProvider, Authenticator, } from '@aws-amplify/ui-react';
 import { I18n } from 'aws-amplify';
 import AmplifyI18n from "amplify-i18n"
-
-import { AuthComponents } from './utils/AuthComponents';
+import { dictPTBR } from './utils/Translations';
 
 import '@aws-amplify/ui-react/styles.css';
 import './App.css'
 
+import { AuthComponents } from './utils/AuthComponents';
 import Main from './pages/Main'
 
 const locales = ["pt-BR"]
 AmplifyI18n.configure(locales)
 I18n.setLanguage("pt-BR")
+I18n.putVocabularies(dictPTBR)
 
 function App() {
-
   return (
     <div className='app-container'>
       <AmplifyProvider >
--- src/utils/Translations.js
+++ src/utils/Translations.js
@@ -1,11 +1,10 @@
-export const dictionaire = {
-    pt: {
+export const dictPTBR = {
+    "pt-BR": {
         'Sign In': 'Entrar',
         'Sign Up': "Inscrever-se",
         'Given Name': 'Nome',
         'Name': 'Sobrenome',
         'We Emailed You': 'Te enviamos um e-mail com o código de confirmação',
-
     },
 }
 
