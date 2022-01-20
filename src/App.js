f4fc7b716d72ba9b68adc54b367bcbb4b512cb7e
Flavio Vieira <flavidev@gmail.com>
2022-01-21 01:02:20 +0400

    added translation to pt-BR

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import { AmplifyProvider, Authenticator, } from '@aws-amplify/ui-react';
+import { I18n } from 'aws-amplify';
 import { AuthComponents } from './utils/AuthComponents';
+
 
 import '@aws-amplify/ui-react/styles.css';
 import './App.css'
 
 import Main from './pages/Main'
+
+import { translations } from '@aws-amplify/ui';
+I18n.putVocabularies(translations);
+I18n.setLanguage('pt-BR');
 
 function App() {
 
@@ -13,8 +19,8 @@ function App() {
       <AmplifyProvider >
         <Authenticator
           components={AuthComponents}
-          signUpAttributes={['name', 'family_name', 'phone_number']}
-          loginMechanisms={['username']}
+          signUpAttributes={['email']}
+          loginMechanisms={['email']}
         >
           {({ signOut, user }) => (
             <Main user={user} signOut={signOut} />
